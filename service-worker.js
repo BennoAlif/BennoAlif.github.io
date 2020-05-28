e91001603e6f5820679b7fc446c530765f669873
BennoAlif <bennoalif41@gmail.com>
2020-05-28 16:03:16 +0700

    Caching pake workbox

--- service-worker.js
+++ service-worker.js
@@ -1,83 +1,41 @@
-const CACHE_NAME = "site-static-v3";
-const urlsToCache = [
-  "./",
-  "./nav.html",
-  "./index.html",
-  "./team.html",
-  "./manifest.json",
-  "./pages/home.html",
-  "./pages/my-favorites.html",
-  "./js/materialize.min.js",
-  "./dist/bundle.js",
-  "./js/idb.js",
-  "./js/template.js",
-  "./assets/bg.jpg",
-  "./assets/no-image.png",
-  "./assets/icons/icon-128x128.png",
-  "./assets/icons/icon-144x144.png",
-  "./assets/icons/icon-152x152.png",
-  "./assets/icons/icon-192x192.png",
-  "./assets/icons/icon-384x384.png",
-  "./assets/icons/icon-512x512.png",
-  "./assets/icons/icon-72x72.png",
-  "./assets/icons/icon-96x96.png",
-];
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+);
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      cache.addAll(urlsToCache);
-    })
-  );
-});
+if (workbox) console.log(`Workbox berhasil dimuat`);
+else console.log(`Workbox gagal dimuat`);
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
-    })
-  );
-});
+workbox.precaching.precacheAndRoute([
+  { url: "/index.html", revision: "1" },
+  { url: "/nav.html", revision: "1" },
+  { url: "/team.html", revision: "1" },
+  { url: "/manifest.json", revision: "1" },
+  { url: "/pages/home.html", revision: "1" },
+  { url: "/pages/my-favorites.html", revision: "1" },
+  { url: "/pages/saved.html", revision: "1" },
+  { url: "/bundle.js", revision: "1" },
+  { url: "/js/materialize.min.js", revision: "1" },
+  { url: "/js/idb.js", revision: "1" },
+  { url: "/assets/bg.jpg", revision: "1" },
+  { url: "/assets/no-image.png", revision: "1" },
+  { url: "/assets/icons/icon-128x128.png", revision: "1" },
+  { url: "/assets/icons/icon-144x144.png", revision: "1" },
+  { url: "/assets/icons/icon-152x152.png", revision: "1" },
+  { url: "/assets/icons/icon-192x192.png", revision: "1" },
+  { url: "/assets/icons/icon-384x384.png", revision: "1" },
+  { url: "/assets/icons/icon-512x512.png", revision: "1" },
+  { url: "/assets/icons/icon-72x72.png", revision: "1" },
+  { url: "/assets/icons/icon-96x96.png", revision: "1" },
+]);
 
-self.addEventListener("fetch", (e) => {
-  const base_url = "https://api.football-data.org/v2/";
-  if (e.request.url.indexOf(base_url) > -1) {
-    e.respondWith(
-      caches.open(CACHE_NAME).then((cache) => {
-        return fetch(e.request).then((res) => {
-          cache.put(e.request.url, res.clone());
-          return res;
-        });
-      })
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request, { ignoreSearch: true }).then((res) => {
-        return res || fetch(e.request);
-      })
-    );
-  }
-});
+workbox.routing.registerRoute(
+  new RegExp("/pages/"),
+  workbox.strategies.staleWhileRevalidate()
+);
 
-self.addEventListener("push", (e) => {
-  let body;
-  if (e.data) {
-    body = e.data.text();
-  } else {
-    body = "Push message no payload";
-  }
-  const options = {
-    body: body,
-    icon: "assets/icons/icon-96x96.png",
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1,
-    },
-  };
-  e.waitUntil(self.registration.showNotification("Notification", options));
-});
+workbox.routing.registerRoute(
+  new RegExp("/pages/"),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "pages",
+  })
+);
